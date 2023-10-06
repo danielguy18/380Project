8d82cc36f35b4ed08b160f3440342379d5fd7851
notbaro <nmtb23@gmail.com>
2023-10-06 03:02:49 -0700

    Added file explorer to File->Open

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,9 +1,24 @@
 package app;
 
+import java.io.File;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
 
 public class Controller {
    @FXML
-   private Circle circle; 
+   private List<Integer> data;
+
+   public void onFileOpenButtonClicked(ActionEvent event) {
+      FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Open Resource File");
+      File file = fileChooser.showOpenDialog(null);
+   }
 }
