e38030e881144a07069250e538c56012335b9102
Zel <zelgehai@gmail.com>
2023-11-21 23:20:57 -0800

    Added Zel-Branch changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# 380Project
-A test repository to learn GIT and to utilize a version control system for a group project in COMP 380
--- src/Util.java
+++
@@ -1,9 +0,0 @@
-public class Util 
-{
-    public int generateConfirmation()
-    {
-        int confirmation = -1;
-        
-        return confirmation;
-    }
-}
