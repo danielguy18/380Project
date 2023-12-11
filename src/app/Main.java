bf8df5246933d7b96f0d6b4dfb24fe70d951fd29
zelgehai <zelgehai@gmail.com>
2023-12-10 18:11:17 -0800

    Added a working editRecord Method to edit mainDatabase.csv

--- src/app/Main.java
+++ src/app/Main.java
@@ -45,8 +45,10 @@ public class Main extends Application {
         String newID = "25";
         String newRoomType = "KING";
         String newPrice = "3000";
-
-        editRecord(datapath,editTerm,newID,newRoomType,newPrice);
+        
+        //Format to edit mainDatabase.csv file 
+        //editRecord(datapath,editTerm,newID,newRoomType,newPrice);
+        //editRecord(datapath,"19", "4000", "QUEENNNN", "5$");  
 
         //Prompt user to launch GUI application or to remain in the console.
         System.out.println("Select an option: \n1. Launch GUI\n2. Remain in console");
